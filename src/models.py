f81cb45a70efe74e645ea1286d061ae151b4015b
Glasaqui <gsanchezquiroz@outlook.com>
2022-09-08 02:06:30 +0000

    Actualiza diagrama

--- src/models.py
+++ src/models.py
@@ -8,24 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table usuario
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    adress1 = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(10), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class FavoritosPer(Base):
+    __tablename__ = 'favoritosperson'
+    # Here we define columns for the table favoritos.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    personaje_id = Column(Integer, ForeignKey("personajes.id"))
+       
+class FavoritosPla(Base):
+    __tablename__ = 'favoritosplanet'
+    # Here we define columns for the table favoritos.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id = Column(Integer, ForeignKey("planetas.id"))
+    
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table favoritos.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gravedad = Column(String(250), nullable=False)
 
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    # Here we define columns for the table favoritos.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    clasificacion = Column(String(250), nullable=False)
+    lenguaje = Column(String(250), nullable=False)
+    creacion = Column(String(250), nullable=False)
+    
     def to_dict(self):
         return {}
 
